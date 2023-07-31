39890dc4583d9372d710bc8fab17194f359adbb1
Victor Soles <victorsoles09@gmail.com>
2023-07-31 10:20:00 -0300

    Sidebar com css

--- resources/js/Layouts/Sidebar.vue
+++ resources/js/Layouts/Sidebar.vue
@@ -4,7 +4,7 @@
         <NavLink :href="route('inicio')">Configurações</NavLink>
         <NavLink :href="route('inicio')">Anotações</NavLink>
     </div> -->
-    <nav class="sidebar">
+    <nav class="sidebar close">
         <header>
             <div class="image-text">
                 <span class="image">
@@ -16,7 +16,60 @@
                     <span class="profession">Supporting</span>
                 </div>
             </div>
+
+            <box-icon name='chevron-right' class="toggle"></box-icon>
         </header>
+
+        <div class="menu-bar">
+            <div class="menu">
+                <ul class="menu-links">
+                    <li class="nav-link">
+                        <a href="#">
+                            <box-icon name='home-alt' class="icon"></box-icon>
+                            <span class="text nav-text">Dashboard</span>
+                        </a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="#">
+                            <box-icon name='note' class="icon"></box-icon>
+                            <span class="text nav-text">Anotações</span>
+                        </a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="#">
+                            <box-icon name='star' class="icon"></box-icon>
+                            <span class="text nav-text">Acesso Rápido</span>
+                        </a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="#">
+                            <box-icon name='cog' class="icon"></box-icon>
+                            <span class="text nav-text">Configurações</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="bottom-content">
+                <li class="">
+                    <a href="#">
+                        <box-icon name='log-out' class="icon"></box-icon>
+                        <span class="text nav-text">Logout</span>
+                    </a>
+                </li>
+                <li class="mode">
+                    <div class="moon-sun">
+                        <box-icon name='moon' class="icon i moon"></box-icon>
+                        <box-icon name='sun' class="icon i sun"></box-icon>
+                    </div>
+
+                    <span class="mode-text text">Dark mode</span>
+
+                    <div class="toggle-switch">
+                        <span class="switch"></span>
+                    </div>
+                </li>
+            </div>
+        </div>
     </nav>
 </template>
 
@@ -27,6 +80,28 @@
         name: 'Sidebar',
         components: {
             NavLink
+        },
+        mounted(){
+            const body = document.querySelector("body"),
+                sidebar = document.querySelector(".sidebar"),
+                toggle = body.querySelector(".toggle"),
+                modeSwitch = body.querySelector(".toggle-switch"),
+                modeText = body.querySelector(".mode-text");
+
+                toggle.addEventListener("click", () =>{
+                    sidebar.classList.toggle("close");
+                });
+
+                modeSwitch.addEventListener("click", () =>{
+                    body.classList.toggle("dark");
+
+                    if(body.classList.contains("dark")){
+                        modeText.innerText = "Ligth Mode"
+                    }else{
+                        modeText.innerText = "Dark Mode"
+                    }
+                });
+
         }
     };
 </script>
