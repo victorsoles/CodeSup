0035b6f13ab670c9efa1605c80aa73e3fad61303
Victor Soles <victorsoles09@gmail.com>
2023-09-25 21:52:38 -0300

    Iniciando CRUD - create

---
+++ app/Http/Controllers/AnotacaoController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Anotacao;
+use App\Http\Requests\StoreAnotacaoRequest;
+use App\Http\Requests\UpdateAnotacaoRequest;
+
+class AnotacaoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAnotacaoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Anotacao $anotacao)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Anotacao $anotacao)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAnotacaoRequest $request, Anotacao $anotacao)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Anotacao $anotacao)
+    {
+        //
+    }
+}
