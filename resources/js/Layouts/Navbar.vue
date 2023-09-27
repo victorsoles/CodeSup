cfe731cc2681aeb19aef798a8425576d9cddbe9f
Victor Soles <victorsoles09@gmail.com>
2023-09-27 08:44:56 -0300

    Add navbar

---
+++ resources/js/Layouts/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <Card class="mt-4">
+        <div class="d-flex justify-content-between align-items-center">
+            <div class="d-xl-none hamburger-button">
+                <box-icon name="menu"></box-icon>
+            </div>
+            <h2 class="text-muted text-center flex-grow-1">Bem vindo!</h2>
+        </div>
+    </Card>
+</template>
+
+<script>
+    import Card from '@/Components/Card.vue';
+
+    export default {
+        name: 'Navbar',
+        components: {
+            Card,
+        }
+    }
+</script>
+
