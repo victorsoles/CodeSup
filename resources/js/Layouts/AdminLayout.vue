b5642a28a465d4a896f74cee08f0af58d10c4473
Victor Soles <victorsoles09@gmail.com>
2023-10-26 08:13:47 -0300

    Possiveis quebras na sidebar

--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -1,47 +1,52 @@
 <template>
     <div class="row no-gutter">
-        <!-- Sidebar -->
-        <div :class="sidebarClass">
-            <Sidebar @estadoSidebar="atualizarSidebar" />
-        </div>
+      <!-- Sidebar -->
+      <div class="d-none d-xl-block col-xl-3 transition-side">
+        <Sidebar @estadoSidebar="atualizarSidebar" @toggle-sidebar="toggleSidebar" />
+      </div>
 
-        <!-- Conteúdo principal -->
-        <div :class="contentClass">
-            <Navbar />
-            <div class="content-wrapper">
-                <div class="container-xxl flex-grow-1 p-0">
-                    <slot />
-                </div>
-            </div>
+      <!-- Conteúdo principal -->
+      <div :class="contentClass">
+        <Navbar @toggleSidebar="toggleSidebar" />
+        <div class="content-wrapper">
+          <div class="container-xxl flex-grow-1 p-0">
+            <slot />
+          </div>
         </div>
+      </div>
     </div>
-  </template>
+</template>
 
-  <script setup>
-  import Sidebar from "@/Layouts/Sidebar.vue";
-  import Navbar from '@/Layouts/Navbar.vue';
-  import { ref, computed } from "vue";
 
-  const sidebarAberta = ref(false); // Inicialmente, a sidebar está fechada
+<script setup>
+import Sidebar from "@/Layouts/Sidebar.vue";
+import Navbar from "@/Layouts/Navbar.vue";
+import { ref, computed } from "vue";
 
-  const atualizarSidebar = (estado) => {
-    sidebarAberta.value = estado;
+const sidebarAberta = ref(false); // Inicialmente, a sidebar está fechada
+
+const atualizarSidebar = (estado) => {
+  sidebarAberta.value = estado;
+};
+
+const sidebarClass = computed(() => {
+  return {
+    "col-xl-3 transition-side": sidebarAberta.value,
   };
+});
 
-  const sidebarClass = computed(() => {
-    return {
-      "d-none d-xl-block col-xl-3 transition-side": sidebarAberta.value,
-      "d-none d-xl-block col-xl-1 transition-side": !sidebarAberta.value,
-    };
-  });
+const contentClass = computed(() => {
+  return {
+    "col-xl-9 col-md-12 transition-side custom-col-9": sidebarAberta.value,
+    "col-xl-11 col-md-12 transition-side custom-col-11": !sidebarAberta.value,
+  };
+});
 
-  const contentClass = computed(() => {
-    return {
-      "col-xl-9 col-md-12 transition-side custom-col-9": sidebarAberta.value,
-      "col-xl-11 col-md-12 transition-side custom-col-11": !sidebarAberta.value,
-    };
-  });
-  </script>
+const toggleSidebar = () => {
+  sidebarAberta.value = !sidebarAberta.value;
+};
+</script>
+
 
   <style scoped>
   /* Estilo personalizado pode ser adicionado aqui, se necessário */
--- resources/js/Layouts/Navbar.vue
+++ resources/js/Layouts/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <Card class="mt-4">
         <div class="d-flex justify-content-between align-items-center">
-            <div class="d-xl-none hamburger-button">
+            <div class="d-xl-none hamburger-button" @click="abreSidebar">
                 <box-icon name="menu"></box-icon>
             </div>
             <h2 class="text-muted text-center flex-grow-1">Bem vindo!</h2>
@@ -16,7 +16,12 @@
         name: 'Navbar',
         components: {
             Card,
-        }
+        },
+        methods: {
+            abreSidebar() {
+                this.$emit('toggleSidebar'); // Emitir um evento para abrir/fechar a Sidebar
+            },
+        },
     }
 </script>
 
